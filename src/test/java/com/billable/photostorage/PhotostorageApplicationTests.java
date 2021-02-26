5b3b5c03289cb899a9c6766a6b6fc8cac9c004f0
sergias <sergias@ukr.net>
2021-02-26 15:14:42 +0200

    Projects starts

---
+++ src/test/java/com/billable/photostorage/PhotostorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billable.photostorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotostorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
